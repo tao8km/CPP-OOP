4f8131047698b362d8fb59b9620b8b02c2a342bc
Nikolai Kostromitin <emailbin@yandex.ru>
2020-11-06 15:48:17 +0500

    Add move constructor and move assignment to Array.

--- src/operator_overloading/Array/Array.cpp
+++ src/operator_overloading/Array/Array.cpp
@@ -1,8 +1,29 @@
 #include "Array.h" // необходимо подключать заголовок класса, чтобы дать компилятору информацию о классе
 
+Array::Array(Array&& other) {
+    *this = other; // использую оператор перемещающего присваивания
+
+    std::cout << "Array(Array&&)\n";
+}
+
+Array& Array::operator= (Array&& other) {
+    if (this != &other) {
+        count = other.count;
+        capacity = other.capacity;
+        elements = other.elements;
+
+        other.count = other.capacity = 0;
+        other.elements = nullptr;
+    }
+
+    std::cout << "operator= (Array&&)\n";
+
+    return *this;
+}
+
 /**
- * Свободная функция (не метод класса Array), перегружающая оператор << для массива.
- * Выводит элементы массива в поток os.
+ * Свободная функция (не метод класса Array), перегружающая оператор << для Array
+ * Выводит элементы Array в поток os.
  */
 std::ostream& operator<<(std::ostream& os, const Array& array) {
     os << '[';
